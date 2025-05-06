e5caca640da97fa3d81f8596f8910dae07cffc4b
pmunozsa <pmunozsa@gmail.com>
2025-05-06 08:19:04 -0400

    Proyecto completo

---
+++ src/main/java/com/hospital_vm/cl/hospital_vm/controller/PacienteController.java
@@ -0,0 +1,31 @@
+package com.hospital_vm.cl.hospital_vm.controller;
+
+import com.hospital_vm.cl.hospital_vm.model.Paciente;
+import com.hospital_vm.cl.hospital_vm.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping
+    public ResponseEntity<List<Paciente>> listar() {
+        List<Paciente> pacientes  = pacienteService.BuscarTodosLosPacientes();
+        if (pacientes.isEmpty()) {
+            return ResponseEntity.noContent().build();
+            //alternativa 2 -> return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return ResponseEntity.ok(pacientes);
+        //alternativa 2 -> return new ResponseEntity<>(pacientes, HttpStatus.OK);
+    }
+
+ 
+}
